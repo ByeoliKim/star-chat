e3692b740c8da180fba83092b79fa780185c0d95
Byeolikim <213069@naver.com>
2022-12-12 22:04:19 +0900

    socket.IO : 채팅방 개설, 공지사항 노출, 상대방 퇴장 알림, 내가 보낸 메시지

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2,14 +2,56 @@ const socket = io();
 
 const welcome = document.getElementById("welcome");
 const form = welcome.querySelector("form");
+const room = document.getElementById("room");
+
+room.hidden = true;
+
+let roomName;
+
+function addMessage(message) {
+    const ul = room.querySelector("ul");
+    const li = document.createElement("li");
+    li.innerText = message;
+    ul.appendChild(li);
+}
+
+function handleMessageSubmit(event) {
+    event.preventDefault();
+    const input = room.querySelector("input");
+    const value = input.value;
+    socket.emit("new_message", input.value, roomName, () => {
+        addMessage(`You ${value}`);
+    });
+    input.value = "";
+    //나는 백엔드로 메시지 "new_message" event 를 보내고 있어!
+}
+
+function showRoom() {
+    //console.log(`The backend says:`, msg);
+    welcome.hidden = true;
+    room.hidden = false;
+    const h3 = room.querySelector("h3");
+    h3.innerText = `Room ${roomName}`;
+    const form = room.querySelector("form");
+    form.addEventListener("submit", handleMessageSubmit);
+}
 
 function handleRoomSubmit(event) {
     event.preventDefault();
     const input = form.querySelector("input");
-    socket.emit("enter_room", {payload:input.value}, () => {
-        console.log("server is done!")
-    }); 
+    socket.emit("enter_room", input.value, showRoom);
+    roomName = input.value;
     input.value = "";
 }
 
 form.addEventListener("submit", handleRoomSubmit);
+
+socket.on("welcome", () => {
+    addMessage("공지사항입니다~ 롤 친구를 찾으세요 ~🤓");
+});
+
+socket.on("bye", () => {
+    addMessage("친구가 방을 나갔어요~ ㅠㅠ");
+});
+
+socket.on("new_message", addMessage);
--- src/server.js
+++ src/server.js
@@ -19,12 +19,34 @@ const httpServer = http.createServer(app);  //http 서버
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
-    socket.on("enter_room", (msg, done) => {
-        console.log(msg);
-        setTimeout(() => {
+    socket.onAny((event) => {
+        console.log(`socket event: ${event}`);
+    });
+    socket.on("enter_room", (roomName, done) => {
+        socket.join(roomName);
+        done(); // done() function 은 프론트에 있는 showRoom () 실행함.
+        socket.to(roomName).emit("welcome");
+        // event 를 방금 참가한 방 안에 있는 모든 사람에게 emit 해 줌.
+        socket.on("disconnecting", () => {
+            socket.rooms.forEach(room => socket.to(room).emit("bye"));
+        });
+        socket.on("new_message", (msg, room, done) => {
+            //어떤 방으로 message 를 보내야 하는지 알 수 있옹~
+            //payload 는 내가 방금 받은 메시지가 될 거야~
+            socket.to(room).emit("new_message", msg);
             done();
-        }, 1000);
-    //console.log(socket);
+            //done 을 호출했을 때, 프론트에서 코드를 실행할 거야~
+        });
+
+        // console.log(socket.id);
+        // console.log(socket.rooms);
+        // console.log(socket.rooms);
+        // setTimeout(() => {
+        //     done("hello from the backend");
+        //     done function 을 실행하면 백엔드에서 이 코드를 실행시키지 않음. (보안 문제)
+        //     프론트에서 실행 버튼을 눌러 주는 거라고 보면 됨.
+        // }, 5000);
+        //console.log(socket);
     });
 });
 
